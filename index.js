6ff605f3387be3371aca1556ba5adc16304631e5
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-11 19:39:53 -0800

    commit tree 188f251efda232b80ed327126f90101758badfb9

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+         'Content-type': 'application/json',
+         'Accept': 'application/json'
+        },
+        body:  JSON.stringify({
+         name: `${name}`,
+         email: `${email}`
+        })
+     })
+     .then(function(res) {
+        return res.json();
+     })
+     .then(function(object) {
+        const idValue = document.createElement('div');
+        idValue.innerHTML = `${object.id}`;
+        document.body.appendChild(idValue);
+        console.log(object);
+     })
+     .catch(function (error) {
+        // alert('Bad things!');
+        const catchMessage = document.createElement('div');
+        catchMessage.innerHTML = 'Unauthorized Access';
+        document.body.appendChild(catchMessage);
+        console.log(error.message);
+     });
+}
